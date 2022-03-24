df6d33a5bc6532c8aebd812597d381aefb28977a
Enrico Ciraci <eciraci@uci.edu>
2022-03-23 17:18:32 -0700

    ddiff_iceye.py - added - Work in Progress

--- ddiff_iceye.py
+++ ddiff_iceye.py
@@ -58,8 +58,15 @@ def main():
     sec_par = os.path.join(data_dir_sec, igram_sec+'.offmap.par.interp')
 
     # - Reference SLCs both interferograms
-    ref_igram_ref = os.path.join(data_dir_ref, igram_ref.split('-')[0])
-    ref_igram_sec = os.path.join(data_dir_sec, igram_sec.split('-')[0])
+    ref_pair_ref = os.path.join(data_dir_ref, igram_ref.split('-')[0])
+    ref_pair_sec = os.path.join(data_dir_sec, igram_sec.split('-')[0])
+    
+    # - Interferograms
+    ref_interf = os.path.join(data_dir_ref,
+                              'coco' + igram_ref + '.flat.topo_off')
+    sec_interf = os.path.join(data_dir_sec,
+                              'coco' + igram_sec + '.flat.topo_off')
+    sec_pwr = os.path.join(data_dir_sec, igram_sec + '.pwr1')
 
     # - Create Output Directory
     out_dir = make_dir(args.directory, 'ddiff')
@@ -71,20 +78,28 @@ def main():
     diff_par = 'DIFF_par' + igram_ref + '--' + igram_sec
     pg.create_diff_par(ref_par, sec_par, diff_par, 0, 0)
     # - Estimate Initial Offset
-    pg.init_offsetm(ref_igram_ref+'.pwr1', ref_igram_sec+'.pwr1',
+    pg.init_offsetm(ref_pair_ref+'.pwr1', ref_pair_sec + '.pwr1',
                     diff_par, 1, 1, '-', '-', '0', '0', '-', '-', 1)
 
     # - Starting Double Difference Computation: Orbits based
-    pg.create_offset(ref_igram_ref + '.par',
-                     ref_igram_sec + '.par',
+    pg.create_offset(ref_pair_ref + '.par',
+                     ref_pair_sec + '.par',
                      igram_ref.split('-')[0] + '-' + igram_sec.split('-')[0]
                      + '.par', 1, 15, 15, 0
                      )
-    pg.init_offset_orbit(ref_igram_ref + '.par',
-                         ref_igram_sec + '.par',
+    pg.init_offset_orbit(ref_pair_ref + '.par',
+                         ref_pair_sec + '.par',
                          igram_ref.split('-')[0] + '-' + igram_sec.split('-')[0]
                          + '.par', '-', '-', 1
                          )
+
+    # - Interpolate Secondary interferogram on the Reference Grid
+    pg.interp_data(sec_interf, diff_par,
+                   sec_interf.split('/')[-1] + '.reg',
+                   1, 1)
+    pg.interp_data(sec_pwr, diff_par,
+                   sec_pwr.split('/')[-1].replace('.pwr1', '.reg.pwr1'),
+                   0, 1)
 
 
 # - run main program
