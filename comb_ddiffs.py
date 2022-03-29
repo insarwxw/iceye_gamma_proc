f07c3f0d4d1e79a28ba6c8b7d75b894818aeb048
Enrico Ciraci <eciraci@uci.edu>
2022-03-29 16:59:55 -0700

    comb_ddiffs.py - added - compine double differences.

--- comb_ddiffs.py
+++ comb_ddiffs.py
@@ -28,7 +28,8 @@ def main():
 
     # - list directory
     dir_1_listr = [os.path.join(dir_1, dr)
-                   for dr in os.listdir(dir_1) if os.path.isdir(os.path.join(dir_1, dr))]
+                   for dr in os.listdir(dir_1) if
+                   os.path.isdir(os.path.join(dir_1, dr))]
     print(dir_1_listr)
 
     for ddiff in dir_1_listr[:2]:
@@ -37,15 +38,15 @@ def main():
         ref_igram = ddiff_name.split('--')[0]
         sec_igram = ddiff_name.split('--')[1]
         ref_slc = ref_igram.split('-')[0]
-        print('# - Geocoded Double Difference: ' + 'coco'+ddiff_name+'.flat.topo_off.geo')
+        print('# - Geocoded Double Difference: ' + 'coco'+ddiff_name+'.flat.topo_off.geo.filt')
         print('# - Reference Interferogram: ' + ref_igram)
         print('# - Secondary Interferogram: ' + sec_igram)
         print('# - Reference SLC: ' + sec_igram)
 
-        ddiff_1 = os.path.join(dir_1, ddiff_name, 'coco'+ddiff_name + '.flat.topo_off.geo')
-        ddiff_2 = os.path.join(dir_2, ddiff_name, 'coco'+ddiff_name + '.flat.topo_off.geo')
-        base_1 = os.path.join(dir_1, ddiff_name, 'base'+ddiff_name + '.flat.topo_off.geo')
-        base_2 = os.path.join(dir_2, ddiff_name, 'base' + ddiff_name + '.flat.topo_off.geo')
+        ddiff_1 = os.path.join(dir_1, ddiff_name, 'coco'+ddiff_name.replace('--', '-') + '.flat.topo_off.geo.filt')
+        ddiff_2 = os.path.join(dir_2, ddiff_name, 'coco'+ddiff_name.replace('--', '-') + '.flat.topo_off.geo.filt')
+        base_1 = os.path.join(dir_1, ddiff_name, 'base'+ddiff_name.replace('--', '-') + '.flat.topo_off.geo')
+        base_2 = os.path.join(dir_2, ddiff_name, 'base' + ddiff_name.replace('--', '-') + '.flat.topo_off.geo')
 
         # - Path to Geocoded Interferogram Parameter Files
         data_dir_ref = os.path.join(intf_dir_1, ref_igram)
@@ -60,8 +61,8 @@ def main():
             dem_nlines = int(read_keyword(ref_par, 'nlines'))
 
         # - Compute Double Difference Difference
-        out_diff = os.path.join(out_dir, 'coco'+ddiff_name + '.flat.topo_off.geo')
-        out_base = os.path.join(out_dir, 'base'+ddiff_name + '.flat.topo_off.geo')
+        out_diff = os.path.join(out_dir, 'coco'+ddiff_name.replace('--', '-') + '.flat.topo_off.geo')
+        out_base = os.path.join(out_dir, 'base'+ddiff_name.replace('--', '-') + '.flat.topo_off.geo')
         pg.comb_interfs(ddiff_1, ddiff_2, base_1, base_2, 1, -1, dem_width,
                         out_diff, out_base)
 
