9c1bbbd7a5d31c91fce72c59e444e909c6412c98
Enrico Ciraci <eciraci@uci.edu>
2023-02-09 10:29:03 -0800

    02/09/2023 - utils/path_to_dem.py - Updated - DEM oversample option added.

--- utils/path_to_dem.py
+++ utils/path_to_dem.py
@@ -1,48 +1,57 @@
+#!/usr/bin/env python
+"""
+Written by Enrico Ciraci' - 12/2022
+Return absolute path to selected DEM and its parameter files.
+"""
 # - Python Dependencies
 import os
 
 
-def path_to_dem(dem: str) -> dict:
+def path_to_dem(dem: str, oversample: int = 1) -> dict:
     """
     Return absolute path to selected DEM and its parameter files.
     :param dem: DEM name.
+    :param oversample: DEM oversampling rate
     :return: dictionary with absolute paths to DEM and its parameter files.
     """
-    # dem_path = os.environ['DAT_PATH']   # - DAT_PATH
-    dem_path = os.environ['PYTHONDATA']   # - DAT_PATH
+    # - Get absolute path to DAT_PATH
+    dem_path = os.environ['DAT_PATH']  # - DAT_PATH
     # - Get absolute path to DEM and its parameter files
     if dem.lower() in ['gis', 'gimp', 'greenland']:
         # - GIMP DEM
         return {'path': os.path.join(dem_path, 'GREENLAND', 'DEM'),
                 'dem': 'gimpdem100.dat',    # - DEM binary
                 'par': 'DEM_gc_par',        # - DEM parameters
-                'oversample': 10,           # - DEM oversampling factor
+                'oversample': oversample,           # - DEM oversampling factor
                 }
-    elif dem.lower() in ['ais', 'antarctica', 'bedmap2']:
+    elif dem.lower() in ['bedmap2']:
         # - BEDMAP2 DEM
-        # dem_path = os.environ['PYTHONDATA']
-        # return {#'path': os.path.join(dem_path, 'ANTARCTICA', 'DEM', 'BEDMAP2'),
-        #         'path': os.path.join(dem_path, 'bedmap2'),
-        #         'dem': 'bedmap2_surface.dat',   # - DEM binary
-        #         'par': 'DEM_gc_par',            # - DEM parameters
-        #         'oversample': 100,              # - DEM oversampling factor
-        #         }
         return {
-            # # - Path to DEM directory
-            # 'path': os.path.join(dem_path, 'ANTARCTICA',
-            #                      'DEM', 'TanDEM-X_500m'),
-            # # 'path': os.path.join(dem_path, 'TanDEM-X_500m'),
-            # # - DEM binary
-            # 'dem': 'TDX_DEM_500m.filtered.ASTER_PEN.BEDMAP2.v2.dat',
-            # 'par': 'DEM_gc_par',            # - DEM parameters
-            # 'oversample': 50,               # - DEM oversampling factor
-            # }
+                'path': os.path.join(dem_path, 'ANTARCTICA', 'DEM', 'BEDMAP2'),
+                'dem': 'bedmap2_surface.dat',   # - DEM binary
+                'par': 'DEM_gc_par',            # - DEM parameters
+                'oversample': oversample,        # - DEM oversampling factor
+                }
+    elif dem.lower() in ['tdx_500m', 'tdx']:
+        return {
+            # - Path to DEM directory
+            'path': os.path.join(dem_path, 'ANTARCTICA',
+                                 'DEM', 'TanDEM-X_500m'),
+            # 'path': os.path.join(dem_path, 'TanDEM-X_500m'),
+            # - DEM binary
+            'dem': 'TDX_DEM_500m.filtered.ASTER_PEN.BEDMAP2.v2.dat',
+            'par': 'DEM_gc_par',            # - DEM parameters
+            'oversample': oversample,       # - DEM oversampling factor
+            }
+    elif dem.lower() in ['rema', 'worldview']:
+        dem_path = os.environ['PYTHONDATA']  # - PYTHONDATA
+        return {
             # - Path to DEM directory
             'path': os.path.join(dem_path, 'REMA'),
             # - DEM binary
             'dem': 'REMA_merge_3031.dat',
             'par': 'DEM_gc_par',  # - DEM parameters
-            'oversample': 3,  # - DEM oversampling factor
+            'oversample': oversample,  # - DEM oversampling factor
             }
     else:
         raise ValueError(f'Unknown DEM selected: {dem}')
