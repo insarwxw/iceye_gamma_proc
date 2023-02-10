2d932fe6aa39cfc86bfaa016c1ab5cbc1b47aeb8
Enrico Ciraci <eciraci@uci.edu>
2023-02-10 11:33:26 -0800

    02/10/2023 - interf_proc.py - Added - Run Interferometric Processing (ISP) on a pair of SLCs - Preliminary Test - Run AMPCOR.

--- interf_proc.py
+++ interf_proc.py
@@ -124,7 +124,7 @@ def main() -> None:
     # - Read ampcor bat file
     with open(os.path.join(out_dir, f'bat_{ref_slc}-{sec_slc}')) as r_fid:
         sub_proc = r_fid.readlines()
-    sub_proc_list = [s.strip()[:2] for s in sub_proc]
+    sub_proc_list = [s.strip(' ')[:2] for s in sub_proc]
     print(sub_proc_list)
 
     # - Run AMPCOR
