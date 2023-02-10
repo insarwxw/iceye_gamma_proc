ac238347b228b57cea3a040d66a66a31a6cc57b8
Enrico Ciraci <eciraci@uci.edu>
2023-02-10 11:07:57 -0800

    02/10/2023 - interf_proc.py - Added - Run Interferometric Processing (ISP) on a pair of SLCs - Preliminary Test - Run AMPCOR.

--- interf_proc.py
+++ interf_proc.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 import os
 import argparse
 import datetime
-import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
 # - GAMMA's Python integration with the py_gamma module
 import py_gamma as pg
 import py_gamma2019 as pg9
@@ -41,6 +42,16 @@ def create_isp_par(data_dir: str, ref: str, sec: str,
         os.path.join(data_dir, f'{sec}.par'),
         os.path.join(data_dir, f'{ref}-{sec}.par')
     )
+
+
+def run_sub_process(cmd: str) -> None:
+    """
+    Run a command in a subprocess
+    :param cmd: command to run
+    :return: None
+    """
+    # - Run the command
+    os.system(cmd)
 
 
 def main() -> None:
@@ -92,18 +103,29 @@ def main() -> None:
     pdoff = args.pdoff         # - Compute preliminary dense offsets field
 
     # - Register SLC-2 to SLC-1 using 3rd order polynomial
-    register_slc(ref_slc, sec_slc,  # - Reference and Secondary SLC
-                 pdoff=pdoff,       # - Compute preliminary dense offsets field
-                 data_dir=data_dir,     # - Path to data directory
-                 out_dir=out_dir)       # - Path to output directory
+    # register_slc(ref_slc, sec_slc,  # - Reference and Secondary SLC
+    #              pdoff=pdoff,       # - Compute preliminary dense offsets field
+    #              data_dir=data_dir,     # - Path to data directory
+    #              out_dir=out_dir)       # - Path to output directory
 
     # - Create the bat file to run AMPCOR
     sec_slc = f'{sec_slc}.reg'  # - Secondary SLC registered to reference SLC
     c_ampcor_iceye(ref_slc, sec_slc,    # - Reference and Secondary SLC
                    data_dir=data_dir,   # - Path to data directory
-                   out_dir=args.directory,  # - Path to output directory
+                   out_dir=out_dir,     # - Path to output directory
                    n_proc=n_proc,       # - Number of parallel processes
                    ampcor=ampcor_bin)   # - AMPCOR binary selected
+
+    print('# - Rin AMPCOR.')
+    # - Read ampcor bat file
+    with open(os.path.join(out_dir, f'bat_{ref_slc}-{sec_slc}')) as r_fid:
+        sub_proc = r_fid.readlines()
+
+    # - Run AMPCOR
+    with Pool(n_proc) as p:
+        p.map(run_sub_process, sub_proc)
+
+    print('# - AMPCOR Run Completed.')
 
 
 # - run main program
